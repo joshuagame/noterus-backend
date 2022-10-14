ac98f4e02fb27554999091fbef3a8aafc25117b4
luca.stasio <luca.stasio@itresources.it>
2022-10-14 10:40:40 +0200

    first commit

---
+++ src/main/java/it/itresources/study/noterusbackend/NoterusBackendApplication.java
@@ -0,0 +1,13 @@
+package it.itresources.study.noterusbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoterusBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoterusBackendApplication.class, args);
+	}
+
+}
